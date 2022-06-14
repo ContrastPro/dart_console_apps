a228d48f89167396ef0d966ffdde1ac695d6694d
Gleb Krasnov <glebon0202@gmail.com>
2022-06-14 17:37:16 +0300

    Modified sample

--- bin/json_convert/models/airline_model.dart
+++ bin/json_convert/models/airline_model.dart
@@ -26,6 +26,24 @@ class AirlineModel {
   final String slogan;
   final String website;
 
+  AirlineModel copyWith({
+    int? id,
+    String? name,
+    String? country,
+    String? logo,
+    String? slogan,
+    String? website,
+  }) {
+    return AirlineModel(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      country: country ?? this.country,
+      logo: logo ?? this.logo,
+      slogan: slogan ?? this.slogan,
+      website: website ?? this.website,
+    );
+  }
+
   Map<String, dynamic> toJson() {
     return {
       'id': id,
